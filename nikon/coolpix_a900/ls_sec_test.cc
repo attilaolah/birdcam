0325addaf2af89c9efa31ab76c6ce5488415c88a
Attila Oláh <atl@google.com>
2022-02-27 15:16:35 +0100

    Add some test cases (expecting to fail.)

--- nikon/coolpix_a900/ls_sec_test.cc
+++ nikon/coolpix_a900/ls_sec_test.cc
@@ -2,4 +2,17 @@
 
 #include "gtest/gtest.h"
 
-TEST(LsSec, Init) { ASSERT_EQ(2 + 2, 4); }
+namespace ls_sec {
+namespace {
+
+TEST(LsSec, LsSec) { const LsSec ls_sec(42); }
+
+TEST(LsSec, stage_1) {
+  LsSec ls_sec(4);
+
+  std::array<uint8_t, 8> exp = {{0, 1, 2, 3, 4, 5, 6, 7}};
+  EXPECT_EQ(ls_sec.stage_1(), exp);
+}
+
+} // namespace
+} // namespace ls_sec
