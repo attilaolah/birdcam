attilaolah/birdcam

commit 1496fed82e5269a0b40b46f244954d6a36d0874d

A lot more debugging.

nikon/ls_sec/entangle.cc
@@ -1,5 +1,6 @@
 #include "entangle.h"
 
+#include <cstdio>  // printf()
 #include <cstdlib>
 #include <limits>
 
@@ -44,12 +45,16 @@ void untangle_2(const KeyMaterial &key, uint32_t *a, uint32_t *b) noexcept {
 }
 
 uint64_t entangle(const std::vector<uint64_t> &elements) noexcept {
-  uint32_t x = 0x1020304;
-  uint32_t y = 0x5060708;
+  uint32_t x0 = 0x1020304;
+  uint32_t y0 = 0x5060708;
 
+  uint32_t x, y;
   for (const uint64_t &elem : elements) {
+    printf("entangle 1. (%#016lx, %#016x, %#016x)\n", elem, x, y);
+    //x = __bswap_32(elem & std::numeric_limits<uint32_t>::max()) ^ x;
     x = __bswap_32(elem & std::numeric_limits<uint32_t>::max()) ^ x;
     y = __bswap_32(elem >> 32) ^ y;
+    printf("entangle 2. (%#016lx, %#016x, %#016x)\n", elem, x, y);
     entangle_2(kKeyMaterial1, &x, &y);
   }
 

nikon/ls_sec/ls_sec.cc
@@ -11,7 +11,9 @@ namespace {
 // Alignment for encoding/decoding.
 constexpr size_t kAlign = sizeof(uint64_t) / sizeof(uint8_t);
 
-uint64_t urand64() { return ((uint64_t)rand() << 32) | rand(); }
+uint64_t urand64() {
+  return __bswap_64(((uint64_t)rand() << 32) | rand());
+}
 
 void populate_key(KeyMaterial &key, uint64_t seed) {
   for (size_t i = 0; i < key.header.size(); i++) {
@@ -70,7 +72,7 @@ uint64_t LsSec::stage_1() {
   }
   stage_ = Stage::STAGE_3;
 
-  return __bswap_64(urand64());
+  return urand64();
 }
 
 std::pair<uint64_t, uint64_t> LsSec::stage_2(uint64_t stage_1) {

nikon/ls_sec/ls_sec.h
@@ -32,7 +32,7 @@ public:
 
   std::vector<uint8_t> decode(const std::vector<uint8_t> &data) const;
 
-private:
+//private:
   const uint64_t hash_3(const uint64_t a, const uint64_t b) const;
 
   void set_secret(const uint64_t nonce, const uint64_t stage_1);

nikon/ls_sec/ref/ref_test.cc
@@ -19,6 +19,7 @@
 extern "C" {
 int64_t LsSecInit(uint8_t *a1, uint64_t a2);
 int64_t LsSec1stStage(uint8_t *a1, uint64_t *a2);
+int64_t LsSec2ndStage(uint8_t *a1, uint64_t *a2, uint64_t *a3, uint64_t *a4);
 }
 
 namespace ls_sec {
@@ -26,6 +27,10 @@ namespace {
 
 constexpr uint64_t kOk = 0;
 constexpr uint64_t kWrongStage = std::numeric_limits<uint32_t>::max() - 102;
+
+constexpr uint64_t kSeed = 1;
+constexpr uint64_t kStage1 = 14277390700105796459ULL;
+constexpr uint64_t kStage2 = 14486366436481708883ULL;
 
 TEST(LsSec, Init) {
   std::vector<uint8_t> buf(8408);
@@ -57,19 +62,48 @@ TEST(LsSec, Stage1) {
 
   EXPECT_EQ(LsSec1stStage(&buf[0], &res), kWrongStage);
 
-  constexpr uint64_t seed = 1;
-  constexpr uint64_t stage_1 = 14277390700105796459ULL;
-
-  LsSecInit(&buf[0], seed);
+  LsSecInit(&buf[0], kSeed);
   EXPECT_EQ(LsSec1stStage(&buf[0], &res), kOk);
-  EXPECT_EQ(res, stage_1);
+  EXPECT_EQ(res, kStage1);
 
-  LsSec ls_sec(seed);
-  EXPECT_EQ(ls_sec.stage_1(), stage_1);
+  LsSec ls_sec(kSeed);
+  EXPECT_EQ(ls_sec.stage_1(), kStage1);
 
   for (const uint8_t& b : buf) {
     EXPECT_EQ(b, (&b - &buf[0] == 8400) ? 3 : 0);
   }
+}
+
+TEST(LsSec, Stage2) {
+  std::pair<uint64_t, uint64_t> res;
+  std::vector<uint8_t> buf(8408);
+
+  uint64_t stage_1 = kStage1;
+  EXPECT_EQ(LsSec2ndStage(&buf[0], &stage_1, &res.first, &res.second), kWrongStage);
+  EXPECT_EQ(stage_1, kStage1);
+
+  LsSecInit(&buf[0], kSeed);
+  EXPECT_EQ(LsSec2ndStage(&buf[0], &stage_1, &res.first, &res.second), kOk);
+  EXPECT_EQ(stage_1, kStage1);
+
+  EXPECT_EQ(res.first, kStage1);
+  EXPECT_EQ(res.second, kStage2);
+
+  LsSec ls_sec(kSeed);
+  const std::pair<uint64_t, uint64_t> stage_2 = ls_sec.stage_2(kStage1);
+  EXPECT_EQ(stage_2.first, kStage1);
+  EXPECT_EQ(stage_2.second, kStage2);
+
+  // index_
+  const uint8_t index = 1;
+  EXPECT_EQ(buf[0], index);
+  // EXPECT_EQ(ls_sec.index_, index); // private
+
+  const uint64_t secret = 7441507252709985025ULL;
+  EXPECT_EQ(ls_sec.secret_, secret);
+  EXPECT_EQ(*reinterpret_cast<uint64_t*>(&buf[0]), secret);
+
+  EXPECT_EQ(buf[8400], 2);
 }
 
 } // namespace

commit ac66e72e3ca5a720a36c956319a892141a0f4c9c

Initialize x and y values to x0 and y0 in entangle()
Changes in commit 1496fed broke entangle() function.
x and y values not only left unitialized and contain random
RAM data on every run, but also should not be zero.

nikon/ls_sec/entangle.cc
@@ -48,7 +48,7 @@ uint64_t entangle(const std::vector<uint64_t> &elements) noexcept {
   uint32_t x0 = 0x1020304;
   uint32_t y0 = 0x5060708;
 
-  uint32_t x, y;
+  uint32_t x = x0, y = y0;
   for (const uint64_t &elem : elements) {
     printf("entangle 1. (%#016lx, %#016x, %#016x)\n", elem, x, y);
     //x = __bswap_32(elem & std::numeric_limits<uint32_t>::max()) ^ x;